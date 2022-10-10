55869eb00297f83c5956db20a92400b8639492fe
sadgh77 <sadeghhadipour1@gmail.com>
2022-10-10 18:09:15 +0330

    add necessary props

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,26 +1,43 @@
 <template>
   <div class="container mx-auto row pt-5">
     <div class="col-6">
-      <div class="border py-2">
+      <div class="border py-2" :class="{'playersTurn':playerTurn === 0}">
         Opponent One : O
       </div>
-      <div class="mt-4">{{ }}</div>
+      <div class="mt-4">{{ playerOneWins }}</div>
     </div>
     <div class="col-6">
-      <div class="border py-2">
+      <div class="border py-2" :class="{'playersTurn':playerTurn === 1}">
         Opponent Two : X
       </div>
-      <div class="mt-4">{{ }}</div>
+      <div class="mt-4">{{ playerTwoWins }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AllScores"
+  name: "AllScores",
+  props: {
+    playerTurn: {
+      type: Number,
+      require: true
+    },
+    playerOneWins: {
+      type: Number,
+      require: true
+    },
+    playerTwoWins: {
+      type: Number,
+      require: true
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.playersTurn {
+  background-color: #25cb20;
+  color: #f9f9f9;
+}
 </style>
