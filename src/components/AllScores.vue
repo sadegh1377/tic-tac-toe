423d7c22b4fb9d6d1611eaf770dea223a9d3f7ee
sadeg1377 <sadeghhadipour1@gmail.com>
2023-07-21 19:00:34 +0330

    fix some bugs with ui

--- src/components/AllScores.vue
+++ src/components/AllScores.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="container mx-auto row py-5">
         <div class="col-6">
-            <div class="border py-2" :class="{'playersTurn':playerTurn === 0}">
+            <div class="border py-2 defaultBackground" :class="{'playersTurn':playerTurn === 0}">
                 {{ opponentOne }}: O
             </div>
             <div class="mt-4">{{ playerOneWins }}</div>
         </div>
         <div class="col-6">
-            <div class="border py-2" :class="{'playersTurn':playerTurn === 1}">
+            <div class="border py-2 defaultBackground" :class="{'playersTurn':playerTurn === 1}">
                 {{ opponentTwo }}: X
             </div>
             <div class="mt-4">{{ playerTwoWins }}</div>
         </div>
-        <router-link to="/" class="btn btn-outline-info btn-sm w-25 mt-3 mx-auto">Back to Home</router-link>
+        <router-link to="/" class="btn btn-outline-info btn-sm w-25 mt-3 mx-auto defaultBackground">Back to Home
+        </router-link>
     </div>
 </template>
 
@@ -45,8 +46,12 @@ export default {
 </script>
 
 <style scoped>
+.defaultBackground {
+    background-color: white;
+}
+
 .playersTurn {
-    background-color: #25cb20;
+    background-color: #25cb20 !important;
     color: #f9f9f9;
 }
 </style>
