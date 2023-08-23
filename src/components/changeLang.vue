e70b5f3612bf34e3b92eff160bca0b3a6e3a8ee8
sadeg1377 <sadeghhadipour1@gmail.com>
2023-08-23 14:47:22 +0330

    give position fixed and add make it to work with localStorage

--- src/components/changeLang.vue
+++ src/components/changeLang.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="changeLang" class="d-flex justify-content-start">
-        <button class="btn border-0 position-absolute" @click="changeLang" title="Change Language">
+    <div class="d-flex justify-content-start changeLang">
+        <button class="btn border-0 position-fixed" @click="changeLang" title="Change Language">
             <img width="40" height="40" src="../assets/language-icon.svg" alt="...">
             <span class="fw-semibold"> {{ selectedLang }}</span>
         </button>
@@ -20,13 +20,24 @@ export default {
     methods: {
         changeLang() {
             if (i18n.locale === 'en') {
+                localStorage.removeItem('lang')
+                localStorage.setItem("lang", 'fa')
                 i18n.locale = 'fa'
-                this.selectedLang = 'fa'
+                this.selectedLang = 'فا'
             } else {
+                localStorage.removeItem('lang')
+                localStorage.setItem("lang", 'en')
                 i18n.locale = 'en'
                 this.selectedLang = 'en'
             }
         },
+    },
+    created() {
+        let lang = localStorage.getItem("lang");
+        if (lang) {
+            this.selectedLang = lang === 'fa' ? 'فا' : 'en'
+            i18n.locale = lang
+        }
     }
 }
 
