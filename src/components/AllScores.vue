dc4deda843733e005d1facf183289f0fc3626a66
sadeg1377 <sadeghhadipour1@gmail.com>
2023-08-23 14:49:36 +0330

    make it to work with i18n

--- src/components/AllScores.vue
+++ src/components/AllScores.vue
@@ -1,38 +1,48 @@
 <template>
     <div class="container mx-auto row py-5">
         <div class="col-6">
-            <div class="border py-2 defaultBackground" :class="{'playersTurn':playerTurn === 0}">
+            <div class="border py-2 defaultBackground"
+                 :class="{'playersTurn':playerTurn === 0, 'changeDir': $i18n.locale === 'fa'}">
                 {{ opponentOne }}: O
             </div>
             <div class="mt-4">{{ playerOneWins }}</div>
         </div>
         <div class="col-6">
-            <div class="border py-2 defaultBackground" :class="{'playersTurn':playerTurn === 1}">
+            <div class="border py-2 defaultBackground"
+                 :class="{'playersTurn':playerTurn === 1 , 'changeDir': $i18n.locale === 'fa'}">
                 {{ opponentTwo }}: X
             </div>
             <div class="mt-4">{{ playerTwoWins }}</div>
         </div>
         <div class="mx-auto d-flex justify-content-center">
             <button class="btn btn-outline-danger btn-sm w-25 mx-3 mt-3 defaultBackground"
-                    data-bs-toggle="modal" data-bs-target="#exampleModal"> Restart
+                    data-bs-toggle="modal" data-bs-target="#exampleModal">
+                {{ $t('allScores.restartBtn') }}
             </button>
-            <router-link to="/" class="btn btn-outline-info btn-sm w-25 mx-3 mt-3 defaultBackground">Back to Home
+            <router-link to="/" class="btn btn-outline-info btn-sm w-25 mx-3 mt-3 defaultBackground">
+                {{ $t('allScores.backToHomeBtn') }}
             </router-link>
         </div>
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-dialog modal-dialog-centered" :class="{'changeDir': $i18n.locale === 'fa'}">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Restart the Game</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title" id="exampleModalLabel">{{ $t('allScores.modalTitle') }}</h5>
+                        <button type="button"
+                                :class="{'changeDirBtn': $i18n.locale === 'fa'}"
+                                class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Are you sure you want to restart the game?
+                        {{ $t('allScores.modalDes') }}
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cansel</button>
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="restartGame">Restart</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            {{ $t('allScores.cancelBtn') }}
+                        </button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="restartGame">
+                            {{ $t('allScores.restartBtn') }}
+                        </button>
                     </div>
                 </div>
             </div>
@@ -81,5 +91,13 @@ export default {
 .playersTurn {
     background-color: #25cb20 !important;
     color: #f9f9f9;
+}
+
+.changeDir {
+    direction: rtl !important;
+}
+
+.changeDirBtn {
+    margin: 0 !important;
 }
 </style>
